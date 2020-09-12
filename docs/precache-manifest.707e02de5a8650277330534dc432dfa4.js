b6fc0994d95aff6965cc094d8435fab295f3454c
EduardoOV <eduardo.oliva@solgeek.com>
2020-09-12 18:16:43 -0500

    docs agregado

---
+++ docs/precache-manifest.707e02de5a8650277330534dc432dfa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e84d52677bd70b86c943d0aff89eb11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fdd66b014939c005ec1",
+    "url": "/static/css/main.22f2c789.chunk.css"
+  },
+  {
+    "revision": "4878dc87bf6dd747ebfe",
+    "url": "/static/js/2.6fa727a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6fa727a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fdd66b014939c005ec1",
+    "url": "/static/js/main.c5648111.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
