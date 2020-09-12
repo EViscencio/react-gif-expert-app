7881a291ff3ae62c48b3619437cdbd141abf9011
EduardoOV <eduardo.oliva@solgeek.com>
2020-09-12 18:15:51 -0500

    Primer commit

---
+++ src/components/AddCategory.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const AddCategory = ({ setCategories }) => {
+    const [value, setValue] = useState('');
+
+    const handleInputChange = e => {
+        setValue(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if (value.trim().length > 2) {
+            setCategories(categories => [value,...categories]);
+            setValue('');
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" onChange={handleInputChange} value={value} />
+        </form>
+    )
+};
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
+};
+
+export default AddCategory;
